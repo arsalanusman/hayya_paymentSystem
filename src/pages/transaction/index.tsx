4d92aeb930d684077082563e1355a08396782e02
arsalanusmanwiz <muhammad.arsalan@wiztech.pk>
2023-11-24 09:58:16 +0500

    updated

--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -30,7 +30,7 @@ const Transaction = () => {
   }, [status == 'paid']);
 
   const sendTransaction = async () => {
-    setIsLoading(false)
+    // setIsLoading(false)
     let request = {
       "transactionId": transId,
       "externalPartyId": id,
@@ -69,17 +69,32 @@ const Transaction = () => {
                       className="text-center mx-auto mb-3"
                     />
                   </div> */}
-                <div className="success-main border-solid  rounded-2xl ">
+                <div className="success-main border-solid text-center rounded-2xl ">
                 {isLoading ? (
                 <>
-                {status == 'Paid' ?   <Image src="/img/account-created-bg2.svg"  width={400}
-                      height={400}
-                      alt="Picture of the author"
-                      className="text-center mx-auto mt-10" /> : 
-                      <Image src="/img/cross.png"  width={180}
-                      height={120}
-                      alt="Picture of the author"
-                      className="text-center mx-auto mt-10" /> }
+                {status == 'Paid' ?  <svg className="center" width="666" height="400" viewBox="0 0 666 666" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect opacity="0.3" x="116.5" y="114.5" width="435" height="435" rx="217.5" stroke="#01A63E"/>
+<rect opacity="0.15" x="0.5" y="0.5" width="665" height="665" rx="332.5" stroke="#01A63E"/>
+<rect opacity="0.7" x="232.5" y="230.5" width="203" height="203" rx="101.5" stroke="#01A63E"/>
+<path d="M333 302L341.482 309.696L352.926 309.253L354.477 320.6L363.529 327.617L357.423 337.306L359.847 348.5L348.941 351.998L343.603 362.13L333 357.8L322.397 362.13L317.059 351.998L306.153 348.5L308.577 337.306L302.471 327.617L311.523 320.6L313.074 309.253L324.518 309.696L333 302Z" fill="#01A63E"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M323 333C323 327.429 327.429 323 333 323C338.571 323 343 327.429 343 333C343 338.571 338.571 343 333 343C327.429 343 323 338.571 323 333ZM337.489 328.15C337.987 328.449 338.149 329.096 337.85 329.594L333.113 337.489C332.946 337.767 332.661 337.953 332.339 337.992C332.017 338.032 331.695 337.921 331.466 337.692L328.308 334.534C327.897 334.123 327.897 333.456 328.308 333.045C328.719 332.634 329.386 332.634 329.797 333.045L332.001 335.25L336.045 328.511C336.344 328.013 336.99 327.851 337.489 328.15Z" fill="#FAF3DD"/>
+</svg>
+: <svg className="center"  width="666" height="400" viewBox="0 0 666 666" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect opacity="0.3" x="116.5" y="114.5" width="435" height="435" rx="217.5" stroke="#d53a3a"/>
+
+
+<rect opacity="0.15" x="0.5" y="0.5" width="665" height="665" rx="332.5" stroke="#d53a3a"/>
+<rect opacity="0.7" x="232.5" y="230.5" width="203" height="203" rx="101.5" stroke="#d53a3a"/>
+
+<path d="M333 302L341.482 309.696L352.926 309.253L354.477 320.6L363.529 327.617L357.423 337.306L359.847 348.5L348.941 351.998L343.603 362.13L333 357.8L322.397 362.13L317.059 351.998L306.153 348.5L308.577 337.306L302.471 327.617L311.523 320.6L313.074 309.253L324.518 309.696L333 302Z" fill="#fff"/>
+
+<path d="M333 302L341.482 309.696L352.926 309.253L354.477 320.6L363.529 327.617L357.423 337.306L359.847 348.5L348.941 351.998L343.603 362.13L333 357.8L322.397 362.13L317.059 351.998L306.153 348.5L308.577 337.306L302.471 327.617L311.523 320.6L313.074 309.253L324.518 309.696L333 302Z" fill="#d53a3a"/>
+
+<line x1="322" y1="322" x2="344" y2="344" stroke="#fff" stroke-width="2" />
+<line x1="344" y1="322" x2="322" y2="344" stroke="#fff" stroke-width="2" />
+</svg>
+
+ }
                       
                      
 
